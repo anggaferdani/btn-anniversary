a7221e62a39239baa61b04370faa67cde48e06d7
anggaferdani <eanggawahyu@gmail.com>
2024-10-08 15:28:12 +0700

    participant

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Participant extends Model
 {
     use HasFactory;
+
+    protected $table = 'participants';
+
+    protected $primaryKey = 'id';
+
+    protected $guarded = [];
 }
