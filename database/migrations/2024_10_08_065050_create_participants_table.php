c853a30e7b2b2474c8db2ac9aba02886b93f4549
LeGamcisco <ahmdrfzn505@gmail.com>
2024-10-09 20:50:48 +0700

    update migrasi participant

--- database/migrations/2024_10_08_065050_create_participants_table.php
+++ database/migrations/2024_10_08_065050_create_participants_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->integer('verification')->default(2);
             $table->integer('attendance')->default(2);
             $table->integer('status')->default(1);
+            $table->integer('point')->default(0);
             $table->timestamps();
         });
     }
