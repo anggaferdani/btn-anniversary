7ff0f5bffa7cd823a36dddfb3b24d84be66bef4b
LeGamcisco <ahmdrfzn505@gmail.com>
2024-10-09 09:45:57 +0700

    update migrasi

--- database/migrations/2024_10_08_065050_create_participants_table.php
+++ database/migrations/2024_10_08_065050_create_participants_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
             $table->string('qrcode')->unique()->nullable();
+            $table->string('token')->unique();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone_number')->nullable();
             $table->integer('verification')->default(2);
             $table->integer('attendance')->default(2);
