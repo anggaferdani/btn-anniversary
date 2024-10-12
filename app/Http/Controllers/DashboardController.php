b5612473046bc0ac4cf54f85e751f39da18f2b4b
LeGamcisco <ahmdrfzn505@gmail.com>
2024-10-13 00:29:08 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Participant;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('backend.pages.dashboard');
+        $participantsCount = Participant::count();
+        $participantsVerifiedCount = Participant::where('verification', 1)->count();
+        $participantsVerifiedOnlineCount = Participant::where('verification', 1)->where('kehadiran', 'online')->count();
+        $participantsVerifiedOfflineCount = Participant::where('verification', 1)->where('kehadiran', 'onsite')->count();
+
+        $participantsAttendanceHistory = Participant::where('attendance', 1)->orderBy('updated_at', 'desc')->get();
+
+        $participants = Participant::where('attendance', 1)->with('instansi')->orderBy('updated_at', 'desc')->get();
+        
+            
+        return view('backend.pages.dashboard', compact('participants', 'participantsCount', 'participantsVerifiedCount', 'participantsVerifiedOnlineCount', 'participantsVerifiedOfflineCount', 'participantsAttendanceHistory'));
     }
 }
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -8,15 +8,204 @@
         Dashboard
       </h2>
     </div>
-    <div class="col-auto ms-auto d-print-none">
-      <div class="btn-list">
-        
-      </div>
-    </div>
   </div>
 </div>
 @endsection
 @section('content')
 <div class="container-xl">
+  <div class="col-auto ms-auto d-print-none">
+    <div class="btn-list">
+      <div class="page-body">
+        <div class="container-xl">
+          <div class="row row-deck row-cards">
+            <div class="col-sm-6 col-lg-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex align-items-center">
+                    <div class="subheader">Jumlah Registrasi Partisipan</div>
+                  </div>
+                  <div class="d-flex align-items-baseline">
+                    <div class="h1 mb-3 me-2">{{ $participantsCount ?? "0" }}</div>
+                  </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card">
+                  <div class="card-body">
+                      <div class="d-flex align-items-center">
+                          <div class="subheader">Partisipan Terverifikasi</div>
+                      </div>
+                      <div class="d-flex align-items-baseline">
+                          <div class="h1 mb-3 me-2">{{ $participantsVerifiedCount ?? "0" }}</div>
+                      </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+            </div>
+          </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex align-items-center">
+                    <div class="subheader">Partisipan Hadir Offline</div>
+                  </div>
+                  <div class="d-flex align-items-baseline">
+                    <div class="h1 mb-3 me-2">{{ $participantsVerifiedOfflineCount ?? "0" }}</div>
+                  </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex align-items-center">
+                    <div class="subheader">Partisipan Belum Hadir Offline</div>
+                  </div>
+                  <div class="d-flex align-items-baseline">
+                    <div class="h1 mb-3 me-2">{{ $participantsVerifiedOnlineCount ?? "0" }}</div>
+                  </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex align-items-center">
+                    <div class="subheader">Jumlah Partisipan Offline</div>
+                  </div>
+                  <div class="d-flex align-items-baseline">
+                    <div class="h1 mb-3 me-2">{{ $participantsVerifiedOfflineCount ?? "0" }}</div>
+                  </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-6">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex align-items-center">
+                    <div class="subheader">Jumlah Partisipan Online</div>
+                  </div>
+                  <div class="d-flex align-items-baseline">
+                    <div class="h1 mb-3 me-2">{{ $participantsVerifiedOnlineCount ?? "0" }}</div>
+                  </div>
+                  <div id="chart-new-clients" class="chart-sm"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">History Kehadiran</h3>
+                </div>
+                <div class="card-body border-bottom py-3">
+                  <div class="d-flex">
+                    <div class="text-secondary">
+                      Show
+                      <div class="mx-2 d-inline-block">
+                        <input type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
+                      </div>
+                      entries
+                    </div>
+                    <div class="ms-auto text-secondary">
+                      Search:
+                      <div class="ms-2 d-inline-block">
+                        <input type="text" class="form-control form-control-sm" aria-label="Search invoice">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table card-table table-vcenter text-nowrap datatable">
+                    <thead>
+                      <tr>
+                        <th class="w-1">
+                          <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices">
+                        </th>
+                        <th class="w-1">No.
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg>
+                        </th>
+                        <th>Nama Partisipan</th>
+                        <th>Instansi</th>
+                        <th>Status</th>
+                        <th>Hadir</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ( $participants as $participant )
+                      <tr>
+                        <td>
+                          <input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice">
+                        </td>
+                        <td><span class="text-secondary">{{ $participant->qrcode }}</span></td>
+                        <td> {{ $participant->name }} </td>
+                        <td>
+                          <span class="flag flag-xs flag-country-us me-2"></span>
+                          {{ $participant->instansi->name }}
+                        </td>
+                        <td>
+                          {{ $participant->attendance ? 'Hadir' : 'Tidak Hadir' }}
+                        </td>
+                        <td>
+                          {{ $participant->updated_at }}
+                        </td>
+                        <td class="text-end">
+                          <span class="dropdown">
+                            <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
+                            <div class="dropdown-menu dropdown-menu-end">
+                              <a class="dropdown-item" href="#">Action</a>
+                              <a class="dropdown-item" href="#">Another action</a>
+                            </div>
+                          </span>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                </table>
+                </div>
+                <div class="card-footer d-flex align-items-center">
+                  <p class="m-0 text-secondary">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
+                  <ul class="pagination m-0 ms-auto">
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+                        prev
+                      </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">4</a></li>
+                    <li class="page-item"><a class="page-link" href="#">5</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">
+                        next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('.datatable').DataTable();
+    });
+</script>
 @endsection
