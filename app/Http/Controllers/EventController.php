97ff4614b98ba758a053a6039f73744e0fe7e9c6
anggaferdani <eanggawahyu@gmail.com>
2024-10-12 14:06:57 +0700

    fix

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Zoom;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function leaderboard() {
         return view('backend.pages.leaderboard');
+    }
+
+    public function onlineEvent() {
+        $zoom = Zoom::first()->get();
+
+        if ($zoom->link == null) {
+            return redirect()->route('index');
+        } else {
+            return redirect()->away($zoom->link);
+        }
     }
 }
